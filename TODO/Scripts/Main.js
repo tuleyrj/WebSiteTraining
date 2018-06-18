b2b38b52c6d648d2dc7e1e2b96d95f3c09562a99
tuleyrj <tuleyrj@gmail.com>
2018-06-18 07:06:44 -0500

    Initial Commit

---
+++ TODO/Scripts/Main.js
@@ -0,0 +1,35 @@
+// Toggle To-Do completion
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+// Remove to-do
+$("ul").on("click", "li span", function(e){
+    // stop the click event from triggering on the <li>
+    e.stopPropagation();
+    console.log($(this).parent());
+    $(this).parent().fadeOut(250, function(){
+        $(this).remove();
+    });
+});
+
+// Add new li
+$("input[type=text]").on("keypress", function(e){
+    if (e.which === 13){
+
+        // Grab the text from the text box;
+        var t = $(this).val();
+
+        // Add a new li element to the list
+        $("ul").append("<li></li>");
+
+        // Add the text to the li (this prevents html injection)
+        $("ul").children().last().text(" " + t);
+
+        // now prepend the span
+        $("ul").children().last().prepend("<span>X</span>");
+
+        // clear the text box
+        $(this).val("");
+    }
+});
